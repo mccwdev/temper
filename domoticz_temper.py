d75e01fce88cf7d7fc9f46ec9d193a603cc73eac
Lennart Jongeneel <lennart@1200wd.com>
2019-03-15 13:57:41 +0100

    Add domoticz update script

---
+++ domoticz_temper.py
@@ -0,0 +1,39 @@
+#
+# Small script to import data from TEMPer sensor and export it to Domoticz
+#
+# This program needs to be run as root, for instance with crontab
+#
+
+import requests
+from temper import Temper
+
+
+# Domoticz settings
+DOMOTICZ_HOST = "domoticz"
+DOMOTICZ_DEVICE_IDX = 6505  # Fill in your Domoticz device ID
+DOMOTICZ_URL = "http://%s:8080" % DOMOTICZ_HOST
+TEMP_CORRECTION = -1.8
+HUM_CORRECTION = 0
+
+
+def main():
+    temper = Temper()
+    temper_data = temper.read()
+
+    # for device in temper_data:
+    device = temper_data[0]
+    temp = device['internal temperature']
+    hum = device['internal humidity']
+    temp_hum = str(temp+TEMP_CORRECTION) + ';' + str(hum+HUM_CORRECTION) + ';0'
+
+    # Update device in Domoticz
+    url = "%s/json.htm?type=command&param=udevice&idx=%d&nvalue=0&svalue=%s" % \
+          (DOMOTICZ_URL, DOMOTICZ_DEVICE_IDX, temp_hum)
+    print(url)
+    resp = requests.get(url)
+    print(resp.text)
+
+
+# Run the main function when the script is executed
+if __name__ == "__main__":
+    main()
