bea9c59a51a1df1e1e1dfe93a74db8b7cf0efe77
Lennart <lennart@1200wd.com>
2020-11-14 14:37:43 +0000

    [ADD] Allow to set temperature correction

--- domoticz_temper.py
+++ domoticz_temper.py
@@ -10,9 +10,11 @@ from temper import Temper
 
 # Domoticz settings
 DOMOTICZ_HOST = "domoticz"
-DOMOTICZ_DEVICE_IDX = 6505  # Fill in your Domoticz device ID
+DOMOTICZ_DEVICE_IDX = 7027  # Fill in your Domoticz device ID
+DOMOTICZ_DEVICE_IDX_EXT = 7026  # Device ID for external temperature Sensor
 DOMOTICZ_URL = "http://%s:8080" % DOMOTICZ_HOST
-TEMP_CORRECTION = -1.8
+TEMP_CORRECTION = 0
+TEMP_EXT_CORRECTION = 2
 HUM_CORRECTION = 0
 
 
@@ -22,16 +24,37 @@ def main():
 
     # for device in temper_data:
     device = temper_data[0]
-    temp = device['internal temperature']
-    hum = device['internal humidity']
-    temp_hum = str(temp+TEMP_CORRECTION) + ';' + str(hum+HUM_CORRECTION) + ';0'
+    temp = device.get('internal temperature')
+    temp_ext = device.get('external temperature')
+    hum = device.get('internal humidity')
+    temp_str = None
+    temp_ext_str = None
+    if temp and hum:
+        temp_str = str(temp+TEMP_CORRECTION) + ';' + str(hum+HUM_CORRECTION) + ';0'
+    elif temp:
+        temp_str = str(temp+TEMP_CORRECTION)
+    if temp_ext:
+        temp_ext_str = str(temp_ext+TEMP_EXT_CORRECTION)
+
 
     # Update device in Domoticz
-    url = "%s/json.htm?type=command&param=udevice&idx=%d&nvalue=0&svalue=%s" % \
-          (DOMOTICZ_URL, DOMOTICZ_DEVICE_IDX, temp_hum)
-    print(url)
-    resp = requests.get(url)
-    print(resp.text)
+    if temp_str:
+        url = "%s/json.htm?type=command&param=udevice&idx=%d&nvalue=0&svalue=%s" % \
+              (DOMOTICZ_URL, DOMOTICZ_DEVICE_IDX, temp_str)
+        print(url)
+        resp = requests.get(url)
+        print(resp.text)
+    else:
+        print("Unknown or no response from device %s" % device)
+
+    # Update external temperature device in Domoticz (if available)
+    if DOMOTICZ_DEVICE_IDX_EXT and temp_ext_str:
+        url = "%s/json.htm?type=command&param=udevice&idx=%d&nvalue=0&svalue=%s" % \
+              (DOMOTICZ_URL, DOMOTICZ_DEVICE_IDX_EXT, temp_ext_str)
+        print(url)
+        resp = requests.get(url)
+        print(resp.text)
+
 
 
 # Run the main function when the script is executed
